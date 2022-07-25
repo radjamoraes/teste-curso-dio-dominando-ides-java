c98aa02d886b431b592cf2982c5014770771d7fa
Radja <radjamoraes@gmail.com>
2022-07-25 15:19:30 -0300

    criação de calculadora

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,15 +11,17 @@ public class PrimeiroPrograma {
 	    
 	    System.out.println(gato);
 	    System.out.println(livros);
+	    
+	}
+
 		
 		/*int a = 2;
 		int b = 3;
 		System.out.println("Hello World!" + (a+b));*/
 	}
 
-}
 
 class Livros{
 	private String nome;
-	private String npag;
+	private String numPaginas;
 }
